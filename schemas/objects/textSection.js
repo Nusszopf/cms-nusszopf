b3d6275ae7fcfab7e3c66cafe0b8d0ca9a937b99
schwamic <ms@tresmo.de>
2020-03-26 14:40:49 +0100

    refactor: change heading types

--- schemas/objects/textSection.js
+++ schemas/objects/textSection.js
@@ -12,9 +12,8 @@ export default {
 					type: 'block',
 					styles: [
 						{ title: 'Normal', value: 'normal' },
-						{ title: 'H1', value: 'h1' },
-						{ title: 'H2', value: 'h2' },
-						{ title: 'H3', value: 'h3' }
+						{ title: 'H3', value: 'h3' },
+						{ title: 'H4', value: 'h4' }
 					],
 					marks: {
 						decorators: [ { title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' } ],
