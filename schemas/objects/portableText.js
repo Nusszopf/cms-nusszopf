f59a057779dfaf350f329eb17f5db5268c282f59
schwamic <ms@tresmo.de>
2020-04-15 11:37:30 +0200

    refactor: add lists to portable text

--- schemas/objects/portableText.js
+++ schemas/objects/portableText.js
@@ -10,6 +10,7 @@ export default {
 				{ title: 'H3', value: 'h3' },
 				{ title: 'H4', value: 'h4' }
 			],
+			lists: [ { title: 'Bullet', value: 'bullet' }, { title: 'Numbered', value: 'number' } ],
 			marks: {
 				decorators: [ { title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' } ],
 				annotations: [ { type: 'link' }, { type: 'internalLink' } ]
